94eaccf2c2c6d6108297b52a475f49a0da7efa9b
Bryan C Guner <bryan.guner@gmail.com>
2023-05-07 12:57:12 -0400

    added featured events EventList to HomePage

--- 4-event-filtering-project/pages/index.js
+++ 4-event-filtering-project/pages/index.js
@@ -1,10 +1,11 @@
 import { getFeaturedEvents } from "../dummy-data";
-
+import EventList from "../components/events/event-list";
 function HomePage() {
+    const featuredEvents = getFeaturedEvents();
   return (
     <div>
       <h1>The Home Page</h1>
-      <ul></ul>
+        <EventList items={featuredEvents} />
     </div>
   );
 }
