e97cf09e18b5cdebb6388aa0bbc1763207952798
Bryan C Guner <bryan.guner@gmail.com>
2023-05-07 12:55:12 -0400

    props set on EventItem in event-list.js

--- 4-event-filtering-project/components/events/event-list.js
+++ 4-event-filtering-project/components/events/event-list.js
@@ -4,7 +4,14 @@ function EventList(props) {
   return (
     <ul className="event-list">
       {props.items.map((event) => (
-        <EventItem key={event.id} id={event.id} title={event.title} />
+        <EventItem
+          key={event.id}
+          id={event.id}
+          title={event.title}
+          location={event.location}
+          date={event.date}
+          image={event.image}
+        />
       ))}
     </ul>
   );
