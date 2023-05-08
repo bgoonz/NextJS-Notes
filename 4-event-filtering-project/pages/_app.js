6cbe35fdbe00324cb5a4fa26bd5678703b74a10e
Bryan C Guner <bryan.guner@gmail.com>
2023-05-08 12:02:07 -0400

    layout wrapper nav component

--- 4-event-filtering-project/pages/_app.js
+++ 4-event-filtering-project/pages/_app.js
@@ -1,7 +1,12 @@
 import "../styles/globals.css";
-
+import Layout from "../components/layout/layout";
+//This is the page content component... we can wrap it with a layout component like a header.
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  );
 }
 
 export default MyApp;
