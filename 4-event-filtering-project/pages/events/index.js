eec842ecdebd1e03ee9acbb634b5a915a8a3d2b2
Bryan C Guner <bryan.guner@gmail.com>
2023-05-08 12:32:35 -0400

    events search component

--- 4-event-filtering-project/pages/events/index.js
+++ 4-event-filtering-project/pages/events/index.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { getAllEvents } from "../../dummy-data";
 import EventList from "../../components/events/event-list";
+import EventsSearch from "../../components/events/events-search";
 function AllEventsPage() {
   const events = getAllEvents();
   return (
-    <React.Fragment>
+      <React.Fragment>
+        <EventsSearch />
       <EventList items={events} />
     </React.Fragment>
   );
