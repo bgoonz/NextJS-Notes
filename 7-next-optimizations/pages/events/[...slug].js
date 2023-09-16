99ee27837743a71104a3150f0419fa8b31541e72
Bryan C Guner <bryan.guner@gmail.com>
2023-09-16 16:30:09 -0400

    fixed error with event filtering using correct url in [...slug].js

--- 7-next-optimizations/pages/events/[...slug].js
+++ 7-next-optimizations/pages/events/[...slug].js
@@ -15,7 +15,7 @@ function FilteredEventsPage(props) {
   const filterData = router.query.slug;
 
   const { data, error } = useSWR(
-    "https://nextjs-course-c81cc-default-rtdb.firebaseio.com/events.json",
+    "https://nextjs-756cd-default-rtdb.firebaseio.com/events.json",
     (url) => fetch(url).then((res) => res.json())
   );
 
