c2549df31a1bd821e51c2307b2b7d4b82271d089
Bryan C Guner <bryan.guner@gmail.com>
2023-10-26 15:17:24 -0400

    inserting comments into the database

--- 9-fullstack-project/pages/api/comments/[eventId].js
+++ 9-fullstack-project/pages/api/comments/[eventId].js
@@ -1,40 +1,33 @@
 // /api/comments/some-event-id
+import { MongoClient } from "mongodb";
 
-function handler(req, res) {
+async function handler(req, res) {
   //the property on the query must match the name of the file (in square brackets)
   const eventId = req.query.eventId;
+
+  const client = await MongoClient.connect("mongodb+srv://bgoonz:Summer2015@cluster0.wagx0yc.mongodb.net/events?retryWrites=true&w=majority");
 
   if (req.method === "POST") {
     const { email, name, text } = req.body;
 
-    if (
-      !email.includes("@") ||
-      !name ||
-      name.trim() === "" ||
-      !text ||
-      text.trim() === ""
-    ) {
+    if (!email.includes("@") || !name || name.trim() === "" || !text || text.trim() === "") {
       res.status(422).json({ message: "Invalid input" });
       return;
     }
 
     const newComment = {
-      id: new Date().toISOString(),
       email,
       name,
       text,
+      eventId
     };
-    console.log(
-      "email",
-      email,
-      "name",
-      name,
-      "text",
-      text,
-      "\n",
-      "newComment",
-      newComment,
-    );
+    
+    const db = client.db();
+   const result = await db.collection('comments').insertOne(newComment);
+    console.log(result);
+    
+    newComment.id = result.insertedId;
+    
     res.status(201).json({ message: "Added comment", comment: newComment });
   }
 
@@ -44,16 +37,17 @@ function handler(req, res) {
         id: "c1",
         name: "Bryan",
 
-        text: "This is a first comment",
+        text: "This is a first comment"
       },
       {
         id: "c2",
         name: "Max",
-        text: "This is a second comment",
-      },
+        text: "This is a second comment"
+      }
     ];
     res.status(200).json({ comments: dummyList });
   }
+  client.close();
 }
 
 export default handler;
