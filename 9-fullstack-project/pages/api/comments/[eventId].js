9150ee883688fa6ab2f29e5692b1d8d43d83d9b3
Bryan C Guner <bryan.guner@gmail.com>
2023-10-26 15:40:36 -0400

    error handling for newsletter api route

--- 9-fullstack-project/pages/api/comments/[eventId].js
+++ 9-fullstack-project/pages/api/comments/[eventId].js
@@ -5,12 +5,20 @@ async function handler(req, res) {
   //the property on the query must match the name of the file (in square brackets)
   const eventId = req.query.eventId;
 
-  const client = await MongoClient.connect("mongodb+srv://bgoonz:Summer2015@cluster0.wagx0yc.mongodb.net/events?retryWrites=true&w=majority");
+  const client = await MongoClient.connect(
+    "mongodb+srv://bgoonz:Summer2015@cluster0.wagx0yc.mongodb.net/events?retryWrites=true&w=majority",
+  );
 
   if (req.method === "POST") {
     const { email, name, text } = req.body;
 
-    if (!email.includes("@") || !name || name.trim() === "" || !text || text.trim() === "") {
+    if (
+      !email.includes("@") ||
+      !name ||
+      name.trim() === "" ||
+      !text ||
+      text.trim() === ""
+    ) {
       res.status(422).json({ message: "Invalid input" });
       return;
     }
@@ -19,7 +27,7 @@ async function handler(req, res) {
       email,
       name,
       text,
-      eventId
+      eventId,
     };
 
     const db = client.db();
@@ -35,9 +43,13 @@ async function handler(req, res) {
     const db = client.db();
     //here .find() without any arguments will return all documents in the collection.
     //sort({ _id: -1 }) this will sort the documents in descending order by id (latest first)
-    const documents = await db.collection("comments").find().sort({ _id: -1 }).toArray();
+    const documents = await db
+      .collection("comments")
+      .find()
+      .sort({ _id: -1 })
+      .toArray();
 
-    res.status(200).json({ comments: documents});
+    res.status(200).json({ comments: documents });
   }
   client.close();
 }
--- 9-fullstack-project/pages/api/newsletter.js
+++ 9-fullstack-project/pages/api/newsletter.js
@@ -1,4 +1,18 @@
 import { MongoClient } from "mongodb";
+
+async function connectDatabase() {
+  const client = await MongoClient.connect(
+    "mongodb+srv://bgoonz:Summer2015@cluster0.wagx0yc.mongodb.net/events?retryWrites=true&w=majority",
+  );
+
+  return client;
+}
+
+async function insertDocument(client, document) {
+  const db = client.db();
+  await db.collection("newsletter").insertOne(document);
+}
+
 async function handler(req, res) {
   if (req.method === "POST") {
     const userEmail = req.body.email;
@@ -6,13 +20,23 @@ async function handler(req, res) {
       res.status(422).json({ message: "Invalid email address." });
       return;
     }
+    let client;
+    try {
+      client = await connectDatabase();
+    } catch (e) {
+      res.status(500).json({ message: "Connecting to the database failed!" });
+      //if this failed we don't want to attempt the next try block so we return from the function.
+      return;
+    }
 
-   const client= await MongoClient.connect(
-      "mongodb+srv://bgoonz:Summer2015@cluster0.wagx0yc.mongodb.net/events?retryWrites=true&w=majority"
-    );
-    const db = client.db();
-    await db.collection("newsletter").insertOne({ email: userEmail });
-    client.close();
+    try {
+      await insertDocument(client, { email: userEmail });
+      client.close();
+    } catch (e) {
+      res.status(500).json({ message: "Inserting data failed!" });
+      return;
+    }
+
     res.status(201).json({ message: "Signed up!" });
   }
 }
