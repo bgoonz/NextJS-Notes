f137fc5a0e26be6672621d2cf597872024a029da
Bryan C Guner <bryan.guner@gmail.com>
2023-11-08 17:56:55 -0500

    notification component

--- 9-fullstack-project/pages/_app.js
+++ 9-fullstack-project/pages/_app.js
@@ -1,5 +1,5 @@
 import Head from "next/head";
-
+import Notification from "./../components/ui/notification";
 import Layout from "../components/layout/layout";
 import "../styles/globals.css";
 
@@ -12,6 +12,7 @@ function MyApp({ Component, pageProps }) {
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
       <Component {...pageProps} />
+      <Notification title="test" message="This is a test" status="pending" />
     </Layout>
   );
 }
