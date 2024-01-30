0fcff6a2d192d0d1000da67c0bfd5162a977f0bf
Bryan C Guner <bryan.guner@gmail.com>
2024-01-30 15:13:53 -0500

    rendering markdown in react

--- 10-building-a-blog/components/posts/post-detail/post-content.js
+++ 10-building-a-blog/components/posts/post-detail/post-content.js
@@ -1,14 +1,14 @@
 import PostHeader from "./post-header";
-import ReactMarkdown from 'react-markdown'
+import ReactMarkdown from "react-markdown";
 import classes from "./post-content.module.css";
 
 const DUMMY_POST = {
   title: "Getting Started",
   image: "getting-started-nextjs.webp",
   content:
-    "## File Based Routing - NextJS uses file based routing - Each file inside the pages directory becomes a route - We can nest files inside the pages directory to create nested routes - We can use the Link component from next/link to navigate between pages - We can use the useRouter hook from next/router to get the current route - We can use the withRouter HOC from next/router to inject the router object into the components props",
+    "## File Based Routing \n- NextJS uses file based routing \n- Each file inside the pages directory becomes a route \n- We can nest files inside the pages directory to create nested routes \n- We can use the Link component from next/link to navigate between pages \n- We can use the useRouter hook from next/router to get the current route \n- We can use the withRouter HOC from next/router to inject the router object into the components props",
   date: "2023-02-10",
-  slug: "getting-started",
+  slug: "getting-started"
 };
 
 function PostContent() {
@@ -16,7 +16,7 @@ function PostContent() {
   return (
     <article className={classes.content}>
       <PostHeader title={DUMMY_POST.title} image={imagePath} />
-      CONTENT
+      <ReactMarkdown>{DUMMY_POST.content}</ReactMarkdown>
     </article>
   );
 }
--- README.md
+++ README.md
@@ -675,3 +675,35 @@ export default handler;
             <Link href="/posts">Posts</Link>
           </li>
 ```
+
+
+---
+
+#### Rendering Markdown In React:
+
+```js
+import PostHeader from "./post-header";
+import ReactMarkdown from "react-markdown";
+import classes from "./post-content.module.css";
+
+const DUMMY_POST = {
+  title: "Getting Started",
+  image: "getting-started-nextjs.webp",
+  content:
+    "## File Based Routing \n- NextJS uses file based routing \n- Each file inside the pages directory becomes a route \n- We can nest files inside the pages directory to create nested routes \n- We can use the Link component from next/link to navigate between pages \n- We can use the useRouter hook from next/router to get the current route \n- We can use the withRouter HOC from next/router to inject the router object into the components props",
+  date: "2023-02-10",
+  slug: "getting-started"
+};
+
+function PostContent() {
+  const imagePath = `/images/posts/${DUMMY_POST.slug}/${DUMMY_POST.image}`;
+  return (
+    <article className={classes.content}>
+      <PostHeader title={DUMMY_POST.title} image={imagePath} />
+      <ReactMarkdown>{DUMMY_POST.content}</ReactMarkdown>
+    </article>
+  );
+}
+
+export default PostContent;
+```
