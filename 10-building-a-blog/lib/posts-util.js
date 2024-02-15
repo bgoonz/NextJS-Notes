2941dceb7d0b50042c4a6eb91754dcd1cbb3b8e4
Bryan C Guner <bryan.guner@gmail.com>
2024-02-15 13:40:24 -0500

    Functions to read and fetch data from markdown

--- 10-building-a-blog/lib/posts-util.js
+++ 10-building-a-blog/lib/posts-util.js
@@ -3,32 +3,27 @@ import path from "path";
 import matter from "gray-matter";
 //process.cwd() points to the root of the project not the lib folder...
 const postsDirectory = path.join(process.cwd(), "posts");
-
 function getPostData(fileName) {
   const filePath = path.join(postsDirectory, fileName);
   const fileContent = fs.readFileSync(filePath, "utf-8");
   const { data, content } = matter(fileContent);
   const postSlug = fileName.replace(/\.md$/, "");
-  const postData = {
-    slug: postSlug,
-    ...data,
-    content,
-  };
-
+  const postData = { slug: postSlug, ...data, content };
   return postData;
 }
 
-function getAllPosts() {
+export function getAllPosts() {
   //postFiles is an array of file name strings
   const postFiles = fs.readdirSync(postsDirectory);
-
   const allPosts = postFiles.map((postFile) => {
     return getPostData(postFile);
   });
-
-  const sortedPosts = allPosts.sort((postA, postB) =>
-    postA.date > postB.date ? -1 : 1,
-  );
-
+  const sortedPosts = allPosts.sort((postA, postB) => (postA.date > postB.date ? -1 : 1));
   return sortedPosts;
+}
+
+export function getFeaturedPosts() {
+  const allPosts = getAllPosts();
+  const featuredPosts = allPosts.filter((post) => post.isfeatured);
+  return featuredPosts;
 }
