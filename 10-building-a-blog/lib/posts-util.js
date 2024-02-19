70177af45a2151ae5191fedf9dede5eba48bec07
Bryan C Guner <bryan.guner@gmail.com>
2024-02-19 14:36:14 -0500

    made getPostData function generic so as to handle either a fileName or a slug as input

--- 10-building-a-blog/lib/posts-util.js
+++ 10-building-a-blog/lib/posts-util.js
@@ -3,11 +3,15 @@ import path from "path";
 import matter from "gray-matter";
 //process.cwd() points to the root of the project not the lib folder...
 const postsDirectory = path.join(process.cwd(), "posts");
-function getPostData(fileName) {
-  const filePath = path.join(postsDirectory, fileName);
+
+
+function getPostData(postIdentifier) {
+    const postSlug = postIdentifier.replace(/\.md$/, "");
+    
+  const filePath = path.join(postsDirectory, `${postSlug}.md`);
   const fileContent = fs.readFileSync(filePath, "utf-8");
   const { data, content } = matter(fileContent);
-  const postSlug = fileName.replace(/\.md$/, "");
+  
   const postData = { slug: postSlug, ...data, content };
   return postData;
 }
